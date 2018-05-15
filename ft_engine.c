6bf5265a54d6ed46d7ffeede5edae178be264cda
malberte <matthieu.albertelli@gmail.com>
2018-05-15 22:02:21 +0200

    Ajout des fonctions liees à la camera

--- ft_engine.c
+++ ft_engine.c
@@ -6,12 +6,14 @@
 /*   By: malberte <malberte@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/05/12 14:19:16 by malberte          #+#    #+#             */
-/*   Updated: 2018/05/13 19:00:47 by malberte         ###   ########.fr       */
+/*   Updated: 2018/05/15 21:32:40 by malberte         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 #include <stdio.h>
+#include <math.h>
+
 #include "ft_engine.h"
 
 void	ft_init_engine(t_engine *engine, int size_x, int size_y, char *title)
@@ -46,4 +48,18 @@ void	ft_init_engine(t_engine *engine, int size_x, int size_y, char *title)
 	engine->col = COLOR_WHITE;
 	engine->size_x = size_x;
 	engine->size_y = size_y;
+}
+
+void ft_init_camera(t_engine *engine, int opening_angle)
+{
+	engine->opening_angle = opening_angle;
+	engine->camera_dist = engine->size_x / (2 * tan(opening_angle / 2));
+	engine->cam_focus[X] = engine->size_x / 2;
+	engine->cam_focus[Y] = engine->size_y / 2;
+}
+
+void ft_perspective_projection(t_pixel pix, const t_engine *e, const t_pos_3d point)
+{
+	pix[X] = e->cam_focus[X] + point[X] * (e->camera_dist / point[Z]);
+	pix[Y] = e->cam_focus[Y] - (point[Y] * (e->camera_dist / point[Z]));
 }
--- ft_engine.h
+++ ft_engine.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_engine.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: acoulomb <acoulomb@student.42.fr>          +#+  +:+       +#+        */
+/*   By: malberte <malberte@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/05/12 13:51:56 by malberte          #+#    #+#             */
-/*   Updated: 2018/05/13 23:39:55 by malberte         ###   ########.fr       */
+/*   Updated: 2018/05/15 21:05:09 by malberte         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,19 +14,27 @@
 # define FT_ENGINE_H
 
 # include <mlx.h>
+# include "ft_position.h"
 
 typedef	struct	s_engine
 {
-	void	*mlx_ptr;
-	void	*win_ptr;
-	int		col;
-	int		size_x;
-	int		size_y;
+	void		*mlx_ptr;
+	void		*win_ptr;
+	int			col;
+	int			size_x;
+	int			size_y;
+	int			opening_angle;
+	int			camera_dist;
+	t_pixel		cam_focus;
 }				t_engine;
 typedef enum	e_color
 {
 	COLOR_WHITE = 16777215
 }				t_color;
-void			ft_init_engine(t_engine *e, int size_x, int size_y, char *tit);
 
+void			ft_init_engine(t_engine *e, int size_x, int size_y, char *tit);
+void			ft_init_camera(t_engine *engine, int opening_angle);
+void			ft_perspective_projection(	t_pixel px,
+											const t_engine *e,
+											const t_pos_3d p);
 #endif
